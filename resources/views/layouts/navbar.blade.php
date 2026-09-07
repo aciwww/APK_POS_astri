bb983f92798e4f41ebaa49b553dab1cc540b7cbc
Rahadirahman21 <rahadi.rahman2007@smkn4-tsm.sch.id>
2026-09-07 11:13:57 +0700

    hapus user

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -9,9 +9,11 @@
         <li class="nav-item">
           <a class="nav-link {{ Request::is('dashboard') ? 'active' : '' }}" aria-current="page" href="{{ route('dashboard') }}">Dashboard</a>
         </li>
+        @if (Auth::user()->role->name === 'admin')
         <li class="nav-item">
           <a class="nav-link {{ Request::is('admin/users') ? 'active' : '' }}" href="{{ route('admin.users') }}">Users</a>
         </li>
+        @endif
         <li class="nav-item">
           <a class="nav-link {{ Request::is('jenis') ? 'active' : '' }}" href="{{ route('jenis.index') }}">Jenis</a>
         </li>
